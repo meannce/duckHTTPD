e47feabc3d1bc03ae3ee1e1f0912d9710856ab30
meannce <optional@tutanota.de>
2025-11-06 20:46:33 +0000

    chore: check sessionId and fingerprint are valid
    part of troubleshooting issue with Havana server

--- src/main/java/org/alexdev/duckhttpd/server/connection/WebConnection.java
+++ src/main/java/org/alexdev/duckhttpd/server/connection/WebConnection.java
@@ -63,6 +63,20 @@ public class WebConnection {
 
     public void validateSession() {
         this.sessionId = CookieSessionManager.getInstance().getSession(this);
+
+        if (this.sessionId == null) {
+        System.err.println("Error: Session ID is null for connection from " + this.getIpAddress());
+        return;
+        }
+
+        String fingerprint = this.sessionId.getFingerprint();
+
+        if (fingerprint != null) {
+            this.cookies().set(CookieSessionManager.HTTPSESSID, fingerprint);
+        } else {
+            System.err.println("Warning: Null fingerprint for session ID. Skipping cookie set.");
+        }
+
         this.cookies().set(CookieSessionManager.HTTPSESSID, this.sessionId.getFingerprint());
 
         this.session = this.sessionId.getWebSession();
